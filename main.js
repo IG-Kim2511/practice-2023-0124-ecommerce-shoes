2b58289d0c84157f8f4e35a35a89dd2e0cc432ea
IG <iikim2511@gmail.com>
2023-01-25 14:43:52 -0500

    before: css : /* header section*/

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 
 // 🍀코딩용... 자동으로 가장 밑으로 스크롤시키기
 
-setTimeout(function(){
-  window.scrollTo(0, document.body.scrollHeight);
-  // window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
-}, 500);
+// setTimeout(function(){
+//   window.scrollTo(0, document.body.scrollHeight);
+//   // window.scrollTo({ left: 0, top: document.body.scrollHeight, behavior: "smooth" });
+// }, 500);
 
 function addToCart(params) {
     
